9b78418bc94002f5cd8df46b414453faa2304164
Steven <stevenxchung@gmail.com>
2018-03-14 20:58:25 -0400

    Adding work from Section 8

---
+++ Section 8 - Introduction to JavaScript/ageCalcScript.js
@@ -0,0 +1,3 @@
+var userAge = prompt("How old are you?");
+var	userAgeInDays = userAge * 365.25;
+alert(userAge + " years is roughly " + userAgeInDays);
---
+++ Section 8 - Introduction to JavaScript/stalkerScript.js
@@ -0,0 +1,5 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var userAge = prompt("What is your age?");
+console.log("Your full name is " + firstName + " " + lastName);
+console.log("You are " + userAge + " years old");
