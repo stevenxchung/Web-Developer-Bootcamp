6f9a39c4385b212ce5326b3c7a889094420a80bb
Steven <stevenxchung@gmail.com>
2018-03-25 21:05:38 -0400

    Added files which may have been missing earlier due to System Restore

--- Section 13 - DOM Manipulation/Selector Exercises Script.js
+++
@@ -1,8 +0,0 @@
-// Exercise 1
-
-// Come up with at least 4 different ways to select the first <p> tag
-var p = document.querySelector("#first")
-var p = document.querySelector(".special")
-var p = document.getElementsByTagName("p")[0]
-var p = document.getElementById("first")
-var p = document.getElementsByClassName("special")[0]
--- Section 17 - Advanced jQuery/jQuery Events - Keypress Script.js
+++
@@ -1,9 +0,0 @@
-// .keypress()
-// Bind an event handler to the "keypress" JavaScript event, or trigger that event on an element.
-
-$("input").keypress(function(event) {
-	if(event.which === 13) {
-		alert("You hit \"enter\"");
-	}
-	console.log(event);
-});
