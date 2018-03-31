e514f945932722d418f887f374b5222dfc96dc6f
Steven <stevenxchung@gmail.com>
2018-03-31 11:52:44 -0400

    Removed duplicate directories

--- Section 8 - Introduction to JavaScript/ageCalcScript.js
+++
@@ -1,3 +0,0 @@
-var userAge = prompt("How old are you?");
-var	userAgeInDays = userAge * 365.25;
-alert(userAge + " years is roughly " + userAgeInDays);
--- Section 8 - Introduction to JavaScript/stalkerScript.js
+++
@@ -1,5 +0,0 @@
-var firstName = prompt("What is your first name?");
-var lastName = prompt("What is your last name?");
-var userAge = prompt("What is your age?");
-console.log("Your full name is " + firstName + " " + lastName);
-console.log("You are " + userAge + " years old");
