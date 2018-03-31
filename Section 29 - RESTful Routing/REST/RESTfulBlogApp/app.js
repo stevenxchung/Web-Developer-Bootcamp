52c3bb7786139c42d87914b427d1eabf74dac070
Steven <stevenxchung@gmail.com>
2018-03-30 22:16:30 -0400

    Adding RESTful Blog App project from Section 29

---
+++ Section 29 - RESTful Routing/REST/RESTfulBlogApp/app.js
@@ -0,0 +1,124 @@
+// Require tools needed
+var bodyParser = require("body-parser"),
+    methodOverride = require("method-override"),
+    expressSanitizer = require("express-sanitizer"),
+    mongoose = require("mongoose"),
+    express = require("express"),
+    app = express();
+    
+// Initialize tools 
+mongoose.connect("mongodb://localhost/restful_blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
+
+// Initialize pattern for blog entries
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+var Blog = mongoose.model("Blog", blogSchema)
+
+// Blog.create({
+//     title: "Test Blog",
+//     image: "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+//     body: "Testing"
+// });
+
+// RESTful Routes
+
+// ROOT
+app.get("/", function(req, res) {
+    res.redirect("/blogs");
+});
+
+// Index Route
+app.get("/blogs", function(req, res) {
+    Blog.find({}, function(err, blogs) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("index", {blogs: blogs});
+        }
+    });
+});
+
+// New Route
+app.get("/blogs/new", function(req, res) {
+    res.render("new");
+});
+
+// Create Route
+app.post("/blogs", function(req, res) {
+    // Sanitize to disable users from running JS
+    console.log(req.body);
+    req.body.blog.body = req.sanitize(req.body.blog.body)
+    console.log("=".repeat(10));
+    console.log(req.body);
+    // Create blog
+    Blog.create(req.body.blog, function(err, newBlog) {
+        if (err) {
+            res.render("new");
+        } else {
+            res.redirect("blogs");
+        }
+    });
+});
+
+// Show Route
+app.get("/blogs/:id", function(req, res) {
+    // Render page to show each blog individually
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if (err) {
+            res.redirect("/blogs");
+        } else {
+            res.render("show", {blog: foundBlog});
+        }
+    });
+});
+
+// Edit Route
+app.get("/blogs/:id/edit", function(req, res) {
+    // Render edit page for the blog
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if (err) {
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+});
+
+// Update Route
+app.put("/blogs/:id", function(req, res) {
+    // Sanitize to disable users from running JS
+    req.body.blog.body = req.sanitize(req.body.blog.body)
+    // Render the specified blog with updates
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
+       if (err) {
+           res.redirect("/blogs");
+       } else {
+           res.redirect("/blogs/" + req.params.id);
+       }
+   });
+});
+
+// Delete Route
+app.delete("/blogs/:id", function(req, res) {
+    // Destroy the specified blog
+    Blog.findByIdAndRemove(req.params.id, function(err) {
+        if (err) {
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs");
+        }
+    });
+});
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("Server is online!");
+});
---
+++ Section 29 - RESTful Routing/REST/readme.md
@@ -0,0 +1,18 @@
+# RESTful Routing
+Here we will learn about RESTful (REpresentational State Transfer) Routing. 
+Based on web results, REST is an "architectural style for providing standards between computer systems on the web, making it easier for systems to communicate with each other."
+For this blog application, we will be implementing a RESTful system.
+In general, the REST architectural style is such that code on the client side can be changed at any time without affecting the operation of the server. Similarly, code on the server side can be changed without affecting the operation of the client.
+Additionally, RESTful systems are stateless i.e. the server does not need to know anything about what state the client is in and vice versa.
+
+For more on REST: https://www.codecademy.com/articles/what-is-rest
+
+## Examples (see RESTfulChart.html for a a more detailed chart)
+Name - Path - HTTP Verb Purpose
+* Index - /dogs - GET - List all dogs
+* New - /dogs/new - GET - Show new dog form
+* Create - /dogs - POST - Create a new dog, then redirect somewhere
+* Show - /dogs/:id - GET - Show info about one specific dog
+* Edit - /dogs/:id/edit - GET - Show edit form for one dog
+* Update - /dogs/:id - PUT - Update a particular dog, then redirect somewhere
+* Destroy - /dogs/:id - DELETE - Delete a particular dog, then redirect somewhere
