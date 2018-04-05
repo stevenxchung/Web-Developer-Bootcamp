752ca07600762d4a146d639f8a3dcc954d14e3a5
Steven <stevenxchung@gmail.com>
2018-04-04 20:28:28 -0400

    Adding version 10.0 of YelpCamp project from Section 36 with UI improvements

---
+++ Section 36 - YelpCamp - UI Improvements/YelpCamp/v10/routes/index.js
@@ -0,0 +1,54 @@
+var express = require("express"),
+    router = express.Router(),
+    passport = require("passport"),
+    User = require("../models/user");
+
+// Render landing page as our root "/" page
+router.get("/", function(req, res) {
+    res.render("landing");
+});
+
+// ======================
+// AUTHENTICATION ROUTES
+// ======================
+
+// Register form
+router.get("/register", function(req, res) {
+    res.render("register");
+});
+
+// Sign up logic
+router.post("/register", function(req, res) {
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function() {
+            res.redirect("/campgrounds");
+        });
+    });
+});
+
+// Login form
+router.get("/login", function(req, res) {
+    res.render("login");
+});
+
+// Login logic
+// Includes passport.authenticate() middleware
+router.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+    }), function(req, res) {
+});
+
+// Logout route
+router.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/campgrounds");
+});
+
+module.exports = router;
